ec7b92788ea9b512f5dd61f1c5f71132cfdf3fe3
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-10-03 18:27:23 -0300

    (feat) Add new carousel of images on homepage

---
+++ src/component/carousel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
+import img1 from '../asset/carousel1.jpg';
+import img2 from '../asset/carousel2.jpg';
+import img3 from '../asset/carousel3.jpg';
+import img4 from '../asset/carousel4.jpg';
+
+const images = [img1, img2, img3, img4];
+const carouselInstance = (props) => (
+  <Carousel>
+    {images.map((item) => {
+      return <Carousel.Item><img className="carousel-img" src={item}/></Carousel.Item>
+    })}
+  </Carousel>
+);
+
+export default carouselInstance;
