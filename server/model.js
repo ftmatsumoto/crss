1eecf31a40e71a24b4f9687f0f9a3902db294477
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-10-19 15:29:02 -0200

    (fix) Fix user model

--- server/model.js
+++ server/model.js
@@ -10,29 +10,6 @@ let emailSchema = new mongoose.Schema({
 // Create a model based on the schema
 let Email = mongoose.model('Email', emailSchema);
 
-// Create a schema
-let userSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  email: { type: String, required: true, unique: true },
-  firstName: { type: String, default: '' },
-  lastName: { type: String, default: '' },
-  address: { type: String, default: '' },
-  status: { type: Boolean, default: false },
-  payment: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment', default: [] }],
-  result: [{
-    wod: { type: mongoose.Schema.Types.ObjectId, ref: 'Wod' },
-    total: { type: Number, required: true },
-    tiebreak: { type: Number, required: false, default: 0 },
-    updated_at: { type: Date, default: Date.now }
-  }],
-  lesson: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lesson', default: [] }],
-  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
-  updated_at: { type: Date, default: Date.now }
-});
-
-// Create a model based on the schema
-let User = mongoose.model('User', userSchema);
-
 let movementSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true, unique: true },
@@ -43,18 +20,17 @@ let movementSchema = new mongoose.Schema({
 
 let Movement = mongoose.model('Movement', movementSchema);
 
-// { type: Array, default: [] }
 let wodSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, default: '' },
-  benchmark: Boolean,
-  typed: { type: String, default: 'other' }, // for time, for reps, emom, tabata
+  typed: { type: String, default: 'other' }, // for time, for reps, for load, emom, tabata
   characteristic: [{
     scheme: { type: String, default: 'other' }, // couplet, triplet, chipper
     repetition: { type: String, default: 'other' }, // low, medium, high
     duration: { type: String, default: 'other' }, // low, medium, high
     load: { type: String, default: 'other' }, // light, medium, heavy
-    modality: { type: String, default: 'other' } // gymnastic, weightlifting, monostructure
+    modality: { type: String, default: 'other' }, // gymnastic, weightlifting, monostructural
+    category: { type: String, default: 'other' } // open, girls, heroes, tournament x, y or z...
   }],
   wod: [{
     exercise: { type: mongoose.Schema.Types.ObjectId, ref:'Exercise' },
@@ -62,19 +38,46 @@ let wodSchema = new mongoose.Schema({
     weight: Number,
     unit: { type: String, default: 'kg' }
   }],
-  totalduration: { type: Number, default: 0 },
+  target: { type: Number, default: 0 }, // if it's for time, this target is 0
   created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   updated_at: { type: Date, default: Date.now }
 });
 
 let Wod = mongoose.model('Wod', wodSchema);
 
+// Create a schema
+let userSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  email: { type: String, required: true, unique: true },
+  firstName: { type: String, default: '' },
+  lastName: { type: String, default: '' },
+  address: { type: String, default: '' },
+  status: { type: Boolean, default: false },
+  payment: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment', default: [] }],
+  result: [{
+    wod: wodSchema,
+    lesson: { type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' },
+    comment: { type: String, required: false, maxLength: 255 },
+    rx: { type: Boolean, default: true },
+    total: { type: Number, required: true },
+    tiebreak: { type: Number, required: false },
+    executed_at: { type: Date, required: true },
+    updated_at: { type: Date, default: Date.now }
+  }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', default: [] }],
+  updated_at: { type: Date, default: Date.now }
+});
+
+// Create a model based on the schema
+let User = mongoose.model('User', userSchema);
+
 let lessonSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
+  category: {type: String, required: false },
   schedule: { type: Date, required: true, unique: true },
   coach: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   wod: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Wod' }],
-  client: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  client: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', unique: true }],
   updated_at: { type: Date, default: Date.now }
 });
 
