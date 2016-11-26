c2e4d2418bb1901a77ba583cbb07caa08a1030e8
FELIPE MATSUMOTO <felipetmatsumoto@gmail.com>
2016-11-26 17:04:03 -0200

    (fix) Change react-tap-event-plugin version

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,47 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import { Link, browserHistory } from 'react-router';
 
 import { Authenticated, NotAuthenticated, LogoutLink, LoginLink } from 'react-stormpath';
 
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import FlatButton from 'material-ui/FlatButton';
+import SvgIcon from 'material-ui/SvgIcon';
+
+import svglogo from '../asset/KI_Cross_Fit_logo.svg';
+
+const containerStyle = {
+  position: 'fixed',
+  height: '60px',
+  width: '100%',
+  zIndex: '100'
+};
+
+const separatorStyle = {
+  margin: '0 0 0 0'
+};
+
 const navbarComponent = () => (
-  <Navbar fixedTop>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="/">CrossFit Ki</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <LinkContainer to="/horarios">
-          <NavItem eventKey={1}>Horarios</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/precos">
-          <NavItem eventKey={2}>Preços</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/equipe">
-          <NavItem eventKey={3}>Equipe</NavItem>
-        </LinkContainer>
-      </Nav>
-      {/*
-
-      <Nav pullRight>
-        <Authenticated>
-          <LogoutLink>Logout</LogoutLink>
-        </Authenticated>
-        <NotAuthenticated>
-          <a href="/login">Login</a>
-        </NotAuthenticated>
-        <NotAuthenticated>
-          <a href="/register">Register</a>
-        </NotAuthenticated>
-      </Nav>
-
-      */}
-    </Navbar.Collapse>
-  </Navbar>
+  <Toolbar style={containerStyle} >
+    <ToolbarGroup firstChild={true} >
+      <a href='/'><img style={{width: '120px', height: '80px'}} src={svglogo} /></a>
+      <ToolbarSeparator style={separatorStyle} />
+      <FlatButton label="Horários" onClick={() => browserHistory.push('/horarios')}/>
+      <ToolbarSeparator style={separatorStyle} />
+      <FlatButton label="Preços" onClick={() => browserHistory.push('/precos')}/>
+      <ToolbarSeparator style={separatorStyle} />
+      <FlatButton label="Equipe" onClick={() => browserHistory.push('/equipe')}/>
+    </ToolbarGroup>
+    <ToolbarGroup lastChild={true} >
+      <NotAuthenticated>
+        <a href='/login'><FlatButton label="Login" /></a>
+      </NotAuthenticated>
+      <Authenticated>
+        <LogoutLink><FlatButton label="Logout" /></LogoutLink>
+      </Authenticated>
+        <ToolbarSeparator style={separatorStyle} />
+      <NotAuthenticated>
+        <a href='/register'><FlatButton label="Register" /></a>
+      </NotAuthenticated>
+    </ToolbarGroup>
+  </Toolbar>
 );
 
 export default navbarComponent;
